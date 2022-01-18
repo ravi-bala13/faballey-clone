4266ca5f78375a574ae07569b00cff09e4b929c7
Sachidanand Pradhan <53556482+Sachi4496@users.noreply.github.com>
2022-01-18 13:15:04 +0530

    working tailwindcss

--- client/src/components/Navbars/Nav/Navbar.jsx
+++ client/src/components/Navbars/Nav/Navbar.jsx
@@ -1,5 +1,18 @@
 export const Navbar = () => {
     return <>
-        navbar
+        <div className="w-full bg-black flex">
+            <div className="flex">
+                <p className="text-xl mx-4 px-3 py-3 hover:bg-orange-600">Faballey</p>
+                <p className="text-xl mx-4 px-3 py-3 hover:bg-orange-600">1</p>
+                <p className="text-xl mx-4 px-3 py-3 hover:bg-orange-600">2</p>
+                <p className="text-xl mx-4 px-3 py-3 hover:bg-orange-600">3</p>
+            </div>
+            <div className="flex">
+                <p className="text-xl mx-4 px-3 py-3 hover:bg-orange-600">Faballey</p>
+                <p className="text-xl mx-4 px-3 py-3 hover:bg-orange-600">1</p>
+                <p className="text-xl mx-4 px-3 py-3 hover:bg-orange-600">2</p>
+                <p className="text-xl mx-4 px-3 py-3 hover:bg-orange-600">3</p>
+            </div>
+        </div>
     </>
 }
