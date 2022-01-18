1ef8ed9816014a7b7e8f8bc22d93414c06f925ee
Bala Murugan R <bals161616@gmail.com>
2022-01-18 15:27:50 +0530

    intial code

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import { Navbar } from "./components/Navbar/Navbar"
+import "./App.css";
+import { Navbar } from "./components/Navbar/Navbar";
+import Products from "./components/Products/Products";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Products></Products>
     </div>
   );
 }
