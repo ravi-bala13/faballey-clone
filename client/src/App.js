ecc879e9142f274b8fd56ea4d3bba0779d35a961
Bala Murugan R <bals161616@gmail.com>
2022-01-18 15:43:32 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
 import "./App.css";
 import { Navbar } from "./components/Navbar/Navbar";
 import Products from "./components/Products/Products";
-=======
-import './App.css';
-import { Navbar } from "./components/Navbars/Nav/Navbar";
->>>>>>> 5a06d7369d3351d1acb61a5073aaeb23c71071ff
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Navbar />
       <Products></Products>
-=======
       <Navbar></Navbar>
-      <h1 className="text-3xl font-bold underline bg-red-500">
-        Hello world!
-      </h1>
->>>>>>> 5a06d7369d3351d1acb61a5073aaeb23c71071ff
+      <h1 className="text-3xl font-bold underline bg-red-500">Hello world!</h1>
     </div>
   );
 }
