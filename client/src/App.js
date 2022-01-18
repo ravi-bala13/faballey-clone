363cc7892a67e805e5a9f4af2683befe2a6366c2
Bala Murugan R <bals161616@gmail.com>
2022-01-18 15:39:08 +0530

    app.js

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+import "./App.css";
+import { Navbar } from "./components/Navbar/Navbar";
+import Products from "./components/Products/Products";
+=======
+import './App.css';
+import { Navbar } from "./components/Navbars/Nav/Navbar";
+>>>>>>> 5a06d7369d3351d1acb61a5073aaeb23c71071ff
+
+function App() {
+  return (
+    <div className="App">
+<<<<<<< HEAD
+      <Navbar />
+      <Products></Products>
+=======
+      <Navbar></Navbar>
+      <h1 className="text-3xl font-bold underline bg-red-500">
+        Hello world!
+      </h1>
+>>>>>>> 5a06d7369d3351d1acb61a5073aaeb23c71071ff
+    </div>
+  );
+}
+
+export default App;
