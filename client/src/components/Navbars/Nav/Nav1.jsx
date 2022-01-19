2a8ee2ad5ffe6237ca827dd965023fc5a5d9bf24
Sachidanand Pradhan <53556482+Sachi4496@users.noreply.github.com>
2022-01-19 09:25:04 +0530

    commit

--- client/src/components/Navbars/Nav/Nav1.jsx
+++ client/src/components/Navbars/Nav/Nav1.jsx
@@ -2,7 +2,25 @@ import { FaChevronRight } from "react-icons/fa";
 import { MdCardTravel } from "react-icons/md";
 import Faballeynav from "../images/logo.png";
 import Indya from "../images/indya.png";
+import 'antd/dist/antd.min.css';
+import { Input } from "antd";
+import React, { useState } from 'react';
+import { Modal, Button } from 'antd';
+
 export const Nav1 = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
     return <>
         <div className="w-full h-10 border border-blue-50 flex">
             <div className="w-2/6 border border-white text-xs font-bold flex mt-3">
@@ -15,10 +33,20 @@ export const Nav1 = () => {
             </div>
             <div className="w-2/6 border border-white">
                 <div className="text-xs font-medium mt-2 float-right mr-6">
-                    <p className="inline-block cursor-pointer">Track Order | Gift Cards | <span>Login</span> | <span>Sign up</span></p>
+                    <p className="inline-block cursor-pointer">Track Order | Gift Cards | <span onClick={showModal}>Login</span> | <span onClick={showModal}>Sign up</span></p>
                     <MdCardTravel className="inline-block ml-2 text-xl cursor-pointer" />
                 </div>
             </div>
         </div>
+        <Modal title="LOGIN OR SIGNUP" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} cancelButtonProps={{ style: { display: 'none' } }} okButtonProps={{ style: { display: 'none' } }}>
+            <div className="w-full border border-yellow-400 py-8">
+                <div className="w-9/12 ml-2">
+                    <label>for a quicker checkout</label>
+                    <input className="px-36 py-4 border border-slate-400" placeholder="Enter Mobile/Email"></input>
+                    <button className="px-48 py-4 bg-pink-600 mt-4"><span className="font-bold text-white">CONTINUE</span></button>
+                    <h5><span>Or continue with</span></h5>
+                </div>
+            </div>
+        </Modal>
     </>
 }
