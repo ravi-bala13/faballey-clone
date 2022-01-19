e7c2ba9ea71b088417414fea73253bd09fb4941d
Bala Murugan R <bals161616@gmail.com>
2022-01-19 10:01:37 +0530

    stash

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
+<<<<<<< Updated upstream
 import { Navbar } from "./components/Navbars/Nav/Navbar";
+=======
+
+>>>>>>> Stashed changes
 import Products from "./components/Products/Products";
+
+import { Navbar } from "./components/Navbars/Nav/Navbar";
 
 function App() {
   return (
@@ -9,7 +15,12 @@ function App() {
       <div>
       </div>
       <Products></Products>
+<<<<<<< Updated upstream
 
+=======
+      {/* <Navbar></Navbar> */}
+      <h1 className="text-3xl font-bold underline bg-red-500">Hello world!</h1>
+>>>>>>> Stashed changes
     </div>
   );
 }
