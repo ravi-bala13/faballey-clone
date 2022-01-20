4cd8537e6a492e6698112cf877aa680233fafd80
Sachidanand Pradhan <53556482+Sachi4496@users.noreply.github.com>
2022-01-20 19:15:06 +0530

    commit

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const signup = require("../models/user.model");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const cookie = require("cookie");
+
+const Author = require("../models/product.model");
+
+router.get("", async (req, res) => {
+    try {
+        const author = Author.find().lean().exec();
+        const cookie = req.cookies.jwt;
+        return res.render("signup", { cookie });
+    } catch (e) {
+        return res.status(500).json({ message: e.message, status: "Failed" });
+    }
+});
+
+router.post("", async (req, res) => {
+    try {
+        let token;
+        const registerUser = new signup({
+            full_Name: req.body.fullName,
+            email: req.body.Email,
+            password: req.body.Password,
+        });
+
+        console.log("signup working", registerUser)
+        // const checkUser = await signup.findOne({ email: email }).lean().exec();
+        // if (checkUser) {
+        //     console.log("You are already a user");
+        //   alert("You are already a user");
+        // } else {
+        token = await registerUser.generateAuthToken();
+        console.log("Token is being generated");
+
+        res.cookie("jwt", token, {
+            expires: new Date(Date.now() + 6000000000000000000),
+            httpOnly: true,
+        });
+        console.log(cookie);
+
+        const register = await registerUser.save();
+        console.log(register);
+
+        return res.redirect("/");
+        // }
+    } catch (e) {
+        return res.status(500).json({
+            message: e.message,
+            status: "error is in this particular block",
+        });
+    }
+});
+
+router.get("/:token", async (req, res) => {
+    try {
+        const user = await signup
+            .find({ tokens: { $elemMatch: { token: req.params.token } } })
+            .lean()
+            .exec();
+        res.status(200).send(user);
+    } catch (e) {
+        return res.status(500).json({ message: e.message, status: "Failed" });
+    }
+});
+
+module.exports = router;
---
+++ server/src/middleweres/auth.js
@@ -0,0 +1,36 @@
+const jasonWt = require('jsonwebtoken');
+const Register = require("../models/user.model");
+
+const auth = async (req, res, next) => {
+    try {
+        console.log("Inside Auth")
+        const token = req.cookies.jwt;
+        console.log("This is my Token " + token);
+
+        if (!token) {
+            return res.redirect('/signup');
+        } else {
+            console.log("This is token" + token);
+            const verifyUser = jasonWt.verify(token, "mynameissurajkarosiafrommasaischool");
+            console.log("This is Verify for User " + verifyUser);
+
+            const user = await Register.findOne({ _id: verifyUser._id });
+            if (!user) {
+                throw new Error()
+            }
+
+            req.token = token
+            req.user = user
+            next()
+
+            req.token = token;
+            req.user = user;
+
+            console.log("This is user Data " + user);
+            next();
+        }
+    } catch (e) {
+        console.log(e);
+    }
+};
+module.exports = auth;
