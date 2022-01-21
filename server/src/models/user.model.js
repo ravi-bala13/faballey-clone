7e8984b9b4a8f55a7764959d250fd38ceab4a7d0
Bala Murugan R <bals161616@gmail.com>
2022-01-21 12:27:36 +0530

    Update user.model.js

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -45,7 +45,7 @@ userSchema.pre("save", function (next) {
   const hash = bcrypt.hashSync(this.password, 10);
   this.password = hash;
   return next();
-})
+});
 
 userSchema.methods.checkPassword = function (password) {
   return new Promise((resolve, reject) => {
@@ -58,5 +58,3 @@ userSchema.methods.checkPassword = function (password) {
 };
 
 module.exports = mongoose.model("user", userSchema);
-
-
