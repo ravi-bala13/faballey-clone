7dd4869a6b291fce1b5e2cb82e45bda5a0b24d36
Bala Murugan R <bals161616@gmail.com>
2022-01-21 21:03:35 +0530

    login user

--- client/src/components/Route/Main.jsx
+++ client/src/components/Route/Main.jsx
@@ -1,4 +1,3 @@
-import { Navbar } from "../Navbars/Nav/Navbar";
 import { Route, Routes } from "react-router-dom";
 import { Products } from "../Products/Products";
 import { ProductDetails } from "../Products/ProductDetails";
@@ -9,7 +8,6 @@ import { Cart } from "../checkout/Cart";
 export const Main = () => {
   return (
     <>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/products" element={<Products />}></Route>
--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const registerSchema = new mongoose.Schema(
   {
-    email: { type: String, required: true, unique: true },
+    mobile: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
   {
