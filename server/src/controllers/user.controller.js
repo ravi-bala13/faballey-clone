e2608485209cb0dcdea8aa81536cd2479d5fdd9c
Sachidanand Pradhan <53556482+Sachi4496@users.noreply.github.com>
2022-01-21 12:04:15 +0530

    some changes in google login

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,31 +1,13 @@
-// const express = require('express');
-
-// const router = express.Router();
-
-// const User = require("../models/user.model")
-
-// const authenticate = require('../middlewares/authenticate')
-
-// router.patch("/update",authenticate,async function (req, res) {
-//        const item = await User.findByIdAndUpdate(req.user._id, req.body, {new: true}).lean().exec();
-//        return res.status(200).send({item}); 
-//     })
-// module.exports = router; 
 const express = require('express');
 
 const router = express.Router();
 
-const User = require('../models/user.model')
+const User = require("../models/user.model")
 
-router.get("", async (req, res) => {
-    try {
-        const user = await User.find().lean().exec();
-        res.status(200).send(user);
-    } catch (err) {
-        res.status(500).json({ message: err.message, status: "Failed" });
-    }
-});
+const authenticate = require('../middlewares/authenticate')
 
-
-
-module.exports = router;
+router.patch("/update",authenticate,async function (req, res) {
+       const item = await User.findByIdAndUpdate(req.user._id, req.body, {new: true}).lean().exec();
+       return res.status(200).send({item}); 
+    })
+module.exports = router; 
--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,8 @@
 const app = require("./index")
-
 const connect = require("./configs/db")
-// require("dotenv").config();
-const port = 2345;
+require("dotenv").config();
+const port = process.env.PORT || 2345;
 app.listen(port, async function () {
     await connect();
     console.log(`listening on port on ${port} `);
-})
+});
