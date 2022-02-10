92d4399a5cdd1cce0274f5573027d02025478695
Sachidanand Pradhan <53556482+Sachi4496@users.noreply.github.com>
2022-02-10 21:26:45 +0530

    commit

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,11 @@
 const app = require("./index");
+
 const connect = require("./configs/db");
+
 require("dotenv").config();
+
 const port = Process.env.PORT || 2345;
+
 app.listen(port, async function () {
   await connect();
   console.log(`listening on port on ${port} `);
